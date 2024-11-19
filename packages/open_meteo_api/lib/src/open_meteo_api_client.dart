43b07dfc67ca3390c7ad839afcc910341f4c0afb
kjs <kjs@cubeoffice.de>
2024-11-19 16:16:44 +0100

    feat(api): add getWeather method to fetch current weather

--- packages/open_meteo_api/lib/src/open_meteo_api_client.dart
+++ packages/open_meteo_api/lib/src/open_meteo_api_client.dart
@@ -22,3 +22,31 @@ Future<Location> locationSearch(String query) async {
 
   return Location.fromJson(results.first as Map<String, dynamic>);
 }
+
+/// Fetches [Weather] for a given [latitude] and [longitude].
+Future<Weather> getWeather({
+  required double latitude,
+  required double longitude,
+}) async {
+  final weatherRequest = Uri.https(_baseUrlWeather, 'v1/forecast', {
+    'latitude': '$latitude',
+    'longitude': '$longitude',
+    'current_weather': 'true'
+  });
+
+  final weatherResponse = await _httpClient.get(weatherRequest);
+
+  if (weatherResponse.statusCode != 200) {
+    throw WeatherRequestFailure();
+  }
+
+  final bodyJson = jsonDecode(weatherResponse.body) as Map<String, dynmaic>;
+
+  if (!bodyJson.containsKey('current_weather')) {
+    throw WeatherNotFoundFailure();
+  }
+
+  final weatherJson = bodyJson['current_weather'] as Map<String, dynamic>;
+
+  return Weather.fromJson(weatherJson);
+}
