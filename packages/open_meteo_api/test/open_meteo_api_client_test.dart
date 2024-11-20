59f7abacec252902de42d7b14dac73b15181c82d
kjs <kjs@cubeoffice.de>
2024-11-20 10:37:16 +0100

    save

---
+++ packages/open_meteo_api/test/open_meteo_api_client_test.dart
@@ -0,0 +1,57 @@
+// In order to have a controlled test environment, mocktail is used to mock the http client
+
+import 'package:http/http.dart' as http;
+import 'package:mocktail/mocktail.dart';
+import 'package:open_meteo_api/open_meteo_api.dart';
+import 'package:test/test.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+class MockResponse extends Mock implements http.Response {}
+
+class FakeUri extends Fake implements Uri {}
+
+void main () {
+  group('OpenMeteoApiClient', () {
+    late http.Client httpClient;
+    late OpenMeteoApiClient apiClient;
+
+    setUpAll(() {
+      registerFallbackValue(FakeUri());
+    });
+
+    setUp(() {
+      httpClient = MockHttpClient();
+      apiClient = OpenMeteoApiClient(httpClient: httpClient);
+    });
+
+    group('constructor', () {
+      test('does not require an httpClient', () {
+        expect(OpenMeteoApiClient(), isNotNull);
+      });
+    });
+
+    group('locationSearch', () {
+      const query = 'mock-query';
+      test('makes correct http request', () async {
+        final response = MockResponse();
+        when(() => response.statusCode).thenReturn(200);
+        when(() => response.body).thenReturn('{}');
+        when(() => httpClient.get(any())).thenAnswer((_) async => response);
+        try {
+          await apiClient.locationSearch(query);
+        } catch (_) {}
+        verify(
+          () => httpClient.get(
+            Uri.https(
+              'geocoding-api.open-meteo.com',
+              '/v1/search',
+              {'name': query, 'count': '1'},
+            ),
+          ),
+        ).called(1)
+      });
+    })
+
+  })
+}
